af98d56c511e6f39a129e30640bab0eddad62cf5
Dustin Alandzes <fezf00@gmail.com>
2023-03-09 20:57:02 -0600

    draco compressed models with types

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
+import checker from "vite-plugin-checker";
 
 export default defineConfig((env) => ({
-    plugins: [react()],
+    plugins: [react(), checker({
+        // e.g. use TypeScript check
+        typescript: true,
+    })],
 }))
