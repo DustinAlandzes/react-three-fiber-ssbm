77877a889dda9d40f6892dcab121de6cf5c9dcaf
Dustin Alandzes <fezf00@gmail.com>
2023-03-09 21:03:27 -0600

    update base for repository

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import checker from "vite-plugin-checker";
 
 export default defineConfig((env) => ({
+    base: "/react-three-fiber-ssbm/",
     plugins: [react(), checker({
         // e.g. use TypeScript check
         typescript: true,
